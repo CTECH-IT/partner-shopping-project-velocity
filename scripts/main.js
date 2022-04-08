629b6ae23455cb81edda79a0fef7362ec11c18de
ColeElbing <8517401@isd535.org>
2022-04-08 11:33:13 -0500

    created a warehouse in main.js, currently bugged ran out of time to fix atm

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-// Put your JavaScript here
+(function(window){
+    'use strict';
+
+    let App = window.App;
+    let WareHouse = App.WareHouse;
+    let DataStore = App.DataStore;
+
+    let myWareHouse = new WareHouse('1', new DataStore());
+    window.myWareHouse = myWareHouse;
+
+})(window);
