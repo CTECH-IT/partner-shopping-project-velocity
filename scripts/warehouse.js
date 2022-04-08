4865a215cd6ae72e8854dba69e5c4e7d2dfe072c
ColeElbing <8517401@isd535.org>
2022-04-08 11:02:00 -0500

    The warehouse script now has createOrder, deliverOrder, and printOrder functions

--- scripts/warehouse.js
+++ scripts/warehouse.js
@@ -18,6 +18,18 @@
         this.db.remove(customerId);
     }
 
+    WareHouse.prototype.printOrders = function() {
+
+        // first, get all the email addresses (keys)
+        let customerIdArray = Object.keys(this.db.getAll());
+
+        console.log('WareHouse#' + this.wareHouseId + ' has pending orders:');
+        //go through the list of emails and get the associated order
+        customerIdArray.forEach(function (id ){
+            console.log(this.db.get(id));
+        }.bind(this));
+    }
+
     App.WareHouse = WareHouse;
     window.App = App;
 
