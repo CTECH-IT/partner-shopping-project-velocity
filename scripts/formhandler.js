9d79467d116de9ebbf8c8a4fc9299d33a5400602
ColeElbing <8517401@isd535.org>
2022-04-18 11:23:19 -0500

    Created the form handler and add a checkout and clear cart button

---
+++ scripts/formhandler.js
@@ -0,0 +1,41 @@
+(function (window) {
+    'use strict';
+
+    let App = window.App || {};
+    let $ = window.jQuery;
+
+    function FormHandler(selector) {
+        if (!selector) {
+            throw new Error('No selector provided!');
+        }
+
+        //find the "selector" in the DOM using jQuery and assign it to this.formElement
+        this.$formElement = $(selector);
+        if (this.$formElement.length == 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+    // Add an event handler for the Submit button and pass in createOrder as a parameter (func)
+    FormHandler.prototype.addSubmitHandler = function (func) {
+        console.log('Setting the submit handler for the form...');
+        this.$formElement.on('submit', function (event) {
+            event.preventDefault();
+
+            // copy all the info from the fields into the variable called data
+            let data = {};
+            $(this).serializeArray().forEach(function (item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value);
+            });
+            console.log(data);
+            func(data); // call the function that was passed in on the data from the form
+
+            this.reset(); //reset the form
+            this.elements[0].focus(); // focus on the first field
+        });
+    };
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+
+})(window);
