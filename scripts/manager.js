03f234a1048386eccc10a7f69f0c49bd9e5a19bb
ColeElbing <8517401@isd535.org>
2022-04-22 11:32:35 -0500

    Made a manage.js file to hosu elal the manager page functions to pull the data from index.html

---
+++ scripts/manager.js
@@ -0,0 +1,13 @@
+(function (window){
+    'use strict'
+
+    let App = window.App || {};
+
+    function Manager(){
+        //Pull all the information from the remote datastore and display it
+    };
+    
+    App.Manger = Manager;
+    window.App = App; 
+
+})(window)
