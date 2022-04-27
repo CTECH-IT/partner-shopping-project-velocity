88c1a18fc333b65c4155b93fab678a27462b8cd6
ColeElbing <8517401@isd535.org>
2022-04-27 11:10:46 -0500

    Finally got the data to transfer over to the manager page

--- scripts/manager.js
+++ scripts/manager.js
@@ -9,9 +9,10 @@ const SERVER_URL = 'http://saturn.rochesterschools.org:8080/json';
     let RemoteDataStore = App.RemoteDataStore;
     let CheckList = App.CheckList;
 
+
+
     let remoteDS = new RemoteDataStore(SERVER_URL);
     window.remoteDS = remoteDS;
-
 
     let myWareHouse = new WareHouse('111', remoteDS);
     window.myWareHouse = myWareHouse;
@@ -19,10 +20,19 @@ const SERVER_URL = 'http://saturn.rochesterschools.org:8080/json';
     // find the checklist that is being updated and create a Checklist object
     let checkList = new CheckList(CHECKLIST_SELECTOR);
 
-    checkList.addRow.call(checkList, remoteDS);
+
+    remoteDS.getAll((data) => {
+        for (let value of Object.values(data)) { //loop through the values remote data objeect
+            if (value.hasOwnProperty("shirtSize")) {
+                checkList.addRow(value) //add checklist row based on data 
+            }
+        }
+    });
 
     checkList.addClickHandler(myWareHouse.deliverOrder.bind(myWareHouse));
 
 
 })(window);
+
+
 
